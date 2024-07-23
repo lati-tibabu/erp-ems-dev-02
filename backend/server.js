c2cbf11fb3da42c7db6c5e1ddb1aac970fe52dbe
Lati Tibabu <lati@kali.latiKali>
2024-07-23 09:59:58 +0300

    server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+const port = 3040;
+const sequelize = require("./config/database");
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Connection has been established successfully.");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}.`);
+        });
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+    });
