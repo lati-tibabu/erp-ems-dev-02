5858ba31ac026bb0095d9bd16f9f2a236783f4df
Lati Tibabu <lati@kali.latiKali>
2024-08-14 06:33:13 +0300

    Update main entry and index styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,18 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 // import './index.css'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <div>Hello</div> */}
-    <RouterProvider router={router} />
+
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+    
   </React.StrictMode>,
 )
 
