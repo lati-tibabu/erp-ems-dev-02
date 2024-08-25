6971c461260da5165521af63903b0d3a5d022e4a
Lati Tibabu <lati@kali.latiKali>
2024-08-25 10:02:17 +0300

    Update frontend core structure and configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+});
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from 'vite'
+// import { createRequire } from 'module'
+// const require = createRequire(
+//     import.meta.url)
+// const Buffer = require('buffer/').Buffer;
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     define: {
+//         'global.Buffer': Buffer,
+//     }
+// });
