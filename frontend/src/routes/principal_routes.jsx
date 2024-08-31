99212fb7e632a419deadc908a8dafa97e58288ed
Lati Tibabu <lati@kali.latiKali>
2024-08-31 22:55:43 +0300

    feat(routes): update principal routes

--- frontend/src/routes/principal_routes.jsx
+++ frontend/src/routes/principal_routes.jsx
@@ -5,7 +5,15 @@ import ProtectedRoute from "../components/protectred_routes";
 
 import Home from "../pages/principal/dashboard-contents/home";
 import Students from "../pages/principal/dashboard-contents/students";
+    import StudentList from "../pages/principal/dashboard-contents/students_pages/student-list";
+    import StudentRegistration from "../pages/principal/dashboard-contents/students_pages/student-registration";
+        import BatchRegistration from "../pages/principal/dashboard-contents/students_pages/batch_registration";
+        import SingleStudentRegistration from "../pages/principal/dashboard-contents/students_pages/single_registration";
+        import StudentSpecificInfo from "../pages/principal/dashboard-contents/students_pages/specific_student_info";
+
+
 import Teachers from "../pages/principal/dashboard-contents/teachers";
+import Classes from "../pages/principal/dashboard-contents/classes";
 import Departments from "../pages/principal/dashboard-contents/departments";
 import Clubs from "../pages/principal/dashboard-contents/clubs";
 import Calendar from "../pages/principal/dashboard-contents/calendar";
@@ -14,14 +22,29 @@ import Profile from "../pages/principal/dashboard-contents/profile";
 import Help from "../pages/principal/dashboard-contents/help";
 
 
-
 const principalRoutes = {
     path: '/principal',
     element: <PrincipalDashboard />,
     children: [
         {path: 'home', element: <Home/>},
-        {path: 'students', element: <Students/>},
+        {path: 'students', element: <Students/>, 
+            children: [
+                {path: 'list', element: <StudentList />},
+                {path: 'registration', element: <StudentRegistration />,
+                    children: [
+                        {path: 'batch', element: <BatchRegistration />},
+                        {path: 'single', element: <SingleStudentRegistration />, 
+                            children: [
+                                {path:'specific/:user_id', element: <StudentSpecificInfo />},
+                            ]
+                        },
+
+                    ]
+                }
+            ]
+        },
         {path: 'teachers', element: <Teachers/>},
+        {path: 'classes', element: <Classes/>},
         {path: 'departments', element: <Departments/>},
         {path: 'clubs', element: <Clubs/>},
         {path: 'calendar', element: <Calendar/>},
