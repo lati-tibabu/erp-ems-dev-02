62b2237d04704df97322835eda66d2029475c40c
Lati Tibabu <lati@kali.latiKali>
2024-08-31 22:55:20 +0300

    feat(frontend): update API interactions and auth slice

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,22 +1,20 @@
-// import { config } from "@fortawesome/fontawesome-svg-core";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import axios from "axios";
 
 const api = axios.create();
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("jwt");
+    (config) => {
+        const token = localStorage.getItem("jwt");
 
-    if (token) {
-      config.headers["authorization"] = `Bearer ${token}`;
+        if (token) {
+            config.headers["authorization"] = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 export default api;
--- frontend/src/store/slices/auth_slices.js
+++ frontend/src/store/slices/auth_slices.js
@@ -2,28 +2,32 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlices = createSlice({
     name: "auth",
-    initialState: { username: "anonymous", role: "none", token: "" },
+    initialState: { username: "anonymous", role: "none", token: "", data: {} },
     reducers: {
         login: (state, action) => {
             state.username = action.payload.username;
             state.role = action.payload.role;
             state.token = action.payload.token;
+            state.data = action.payload.data;
 
             localStorage.setItem("username", action.payload.username);
             localStorage.setItem("role", action.payload.role);
             localStorage.setItem("token", action.payload.token);
+            localStorage.setItem("data", JSON.stringify(action.payload.data));
         },
         logout: (state) => {
             state.username = "anonymous";
             state.role = "none";
             state.token = "";
+            state.data = {};
 
             localStorage.removeItem("username");
             localStorage.removeItem("role");
             localStorage.removeItem("token");
+            localStorage.removeItem("data");
         },
     },
 });
 
-export const { login, logout } = authSlices.actions;
+export const { login, logout, schoolLogin } = authSlices.actions;
 export default authSlices.reducer;
